5dfeff415c40e812c76a95377e1b239117e14ba1
Andreotti <j.andreoti59@laposte.net>
2018-04-25 11:49:44 +0200

    deuxieme commit

---
+++ eclipse-workspace/Exo_CGI/src/Methodes.java
@@ -0,0 +1,9 @@
+
+public class Methodes {
+	/**
+	 * Méthode qui calcule la somme de deux parametres entiers
+	 */
+	public int somme(int a, int b) {
+		return a + b;
+	}
+}
---
+++ eclipse-workspace/Exo_CGI/src/Somme.java
@@ -0,0 +1,12 @@
+
+public class Somme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 8;
+		Methodes methodes = new Methodes();
+		System.out.println("La somme est de " + methodes.somme(a, b));
+	}
+
+}
